82f434b1ccccf6cd63c9d567de2396a02c52fa45
Havid <great_havid@yahoo.co.id>
2019-11-11 09:30:44 +0700

    Tugas 1 JavaScript
    Bootcamp niomic

---
+++ tugas1.js
@@ -0,0 +1,10 @@
+// ====== Tugas 1 JavaScript =====
+
+var Nama_depan = "Havid";
+let Nama_akhir = "Riyono";
+const umur = 25;
+
+console.log(Nama_depan);
+console.log(Nama_akhir);
+console.log(umur);
+
